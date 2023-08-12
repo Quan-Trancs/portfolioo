883fb7cb875d750cf5febb3195a94bf6610a080f
QuanTA18 <unconfigured@null.spigotmc.org>
2023-08-11 22:01:15 -0700

    update skills section UI [QuanTran]

--- components/sections/Skills.vue
+++ components/sections/Skills.vue
@@ -1,18 +1,54 @@
 <script setup>
+import {ref} from "vue";
+
+const skillView = ref(['HTML/CSS', 'JavaScript', 'vue.js', 'nuxt3'])
+const frontEnd = ['HTML/CSS', 'JavaScript', 'vue.js', 'nuxt3']
+const backEnd = ['Java', 'Spring Boot']
+const database = ['SQL']
+function viewSkill (skillType) {
+  if (skillType === 'front') {
+    skillView.value = frontEnd
+  } else if (skillType === 'back') {
+    skillView.value = backEnd
+  } else {
+    skillView.value = database
+  }
+}
 </script>
 
 <template>
   <div>
-    <div class="sectionTitle">TECHNICAL SKILLS</div>
-    <div class="skillList">
-      <button>front-end</button>
-      <button>back-end</button>
-      <button>database</button>
+    <link href="https://fonts.cdnfonts.com/css/atari" rel="stylesheet">
+    <div class="sectionTitle" style="font-family: 'Atari', sans-serif; text-shadow: 0 0 20px limegreen, 0 0 30px darkgreen">
+      TECHNICAL SKILLS
     </div>
+    <div class="skillList">
+      <button @click="viewSkill('front')">front-end</button>
+      <button @click="viewSkill('back')">back-end</button>
+      <button @click="viewSkill('data')">database</button>
+    </div>
+    <ul class="skills">
+      <li v-for="skill in skillView">
+        {{ skill }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <style scoped>
+.skills {
+  display: flex;
+  justify-content: center;
+  height: 100px;
+  font-size: 20px; /* 14px */
+  line-height: 100px; /* 20px */
+  margin-top: 40px;
+}
+.skills li {
+  margin-inline: 40px;
+  color: white;
+  font-family: 'Atari', sans-serif;
+}
 .skillList {
   display: flex;
   justify-content: center;
@@ -25,12 +61,14 @@
   border: none;
   color: white;
   font-size: 32px;
+  font-family: Atari, sans-serif;
   margin-inline: 140px;
 }
 .skillList button:hover{
-  border: 10px solid green;
+  text-shadow: 0 0 30px green, 0 0 20px darkgreen;
 }
 .skillList button:focus{
-  border: 10px solid white;
+  border: 7px solid white;
+  padding-inline: 30px;
 }
 </style>
