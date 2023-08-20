d7e5a54603ab813437abf1dfc6fee9b5ef25f77d
QuanTA18 <unconfigured@null.spigotmc.org>
2023-08-20 01:28:13 -0700

    add Logo [QuanTran]

--- components/layouts/TheHeader.vue
+++ components/layouts/TheHeader.vue
@@ -2,9 +2,10 @@
 </script>
 
 <template>
-  <div class="header h-[200px] bg-blue px-2 py-2.5 py-5 text-white sm:px-4">
+  <div class="header bg-blue px-2 py-2.5 py-5 text-white sm:px-4 z-5">
     <div class="container mx-auto flex flex-wrap items-center">
       <ul class="navigation">
+        <li><a href="#about" class="logo"><img src="static/logo/logo.jpg" alt="AQ" /></a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#skills">Skills</a></li>
         <li><a href="#projects">Projects</a></li>
@@ -17,19 +18,31 @@
 .header {
   position: sticky;
   top: 0;
+  z-index: 10;
 }
 .container {
-  height: 120px;
+  height: 110px;
   background: white;
 }
+.logo {
+  font-size: 70px;
+  color: rebeccapurple;
+}
 .navigation {
-  margin-top: 40px;
   display: flex;
-  list-style-type: none;
+  list-style: none;
+  justify-content: space-around;
+  background-color: white;
   font-size: 40px;
+  height: 100px;
 }
 .navigation li {
-  margin-right: 30px;
   text-decoration: none;
+  margin: auto;
+}
+.navigation a {
+  text-decoration: none;
+  color: black;
+  font-family: 'Atari', sans-serif;
 }
 </style>
